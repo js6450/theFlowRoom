db6c6289e1e59c1279d6f93a61a8e8016e9016c0
Jiwon <js6450@nyu.edu>
2018-11-01 20:27:59 -0400

    added missing view folder in server

---
+++ server/public/js/Body.js
@@ -0,0 +1,14 @@
+class Body {
+  constructor() {
+    this.group = new THREE.Group();
+    this.position;
+    this.scale;
+    this.joints;
+    this.lights = [];
+    this.color;
+
+    //this.pointCloud;
+    //this.pointsMax = [];
+    //this.lines = [];
+  }
+}
